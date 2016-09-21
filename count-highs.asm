61483382b34d76c06dcabd349f5ff3ba0096d263
George Z. Zachos <gzachos@cse.uoi.gr>
2016-09-21 14:39:27 +0300

    Fixed indentation in count-highs.asm

--- count-highs.asm
+++ count-highs.asm
@@ -8,25 +8,25 @@
 	.text
 	
 main:
-	li $a0, 0x7a  # 0x7a == 0b01111010
-	jal count_highs
+	li	$a0, 0x7a   # 0x7a == 0b01111010
+	jal	count_highs
 	
-	li $v0, 10    # service code 10: exit
+	li	$v0, 10   # service code 10: exit
 	syscall
 
 count_highs:
-	add $v0, $zero, $zero  # $v0 = 0
-	addi $sp, $sp, -4      # adjust stack pointer to store $a0
-	sw $a0, 0($sp)         # Mem[$sp] = $a0
+	add	$v0, $zero, $zero   # $v0 = 0
+	addi	$sp, $sp, -4        # adjust stack pointer to store $a0
+	sw	$a0, 0($sp)         # Mem[$sp] = $a0
 loop:
-	beq $a0, $zero, exit_sub # if ($a0 == 0) goto exit_sub
-	andi $t0, $a0, 0x01      # $t0 = $a0 & 1
-	srl $a0, $a0, 1          # $a0 = $a0 >> 1
-	beq $t0, $zero, loop     # if ($t0 == 0) goto loop
-	addi $v0, $v0, 1         # $v0 += 1
+	beq	$a0, $zero, exit_sub  # if ($a0 == 0) goto exit_sub
+	andi	$t0, $a0, 0x01        # $t0 = $a0 & 1
+	srl	$a0, $a0, 1           # $a0 = $a0 >> 1
+	beq	$t0, $zero, loop      # if ($t0 == 0) goto loop
+	addi	$v0, $v0, 1           # $v0 += 1
 	j loop
 exit_sub:
-	lw $a0, 0($sp)    # $a0 = Mem[$sp]
-	addi $sp, $sp, 4  # adjust stack pointer
-	jr $ra
+	lw	$a0, 0($sp)   # $a0 = Mem[$sp]
+	addi	$sp, $sp, 4   # adjust stack pointer
+	jr	$ra
 	
